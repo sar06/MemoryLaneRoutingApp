e1fbce66ea4e5cb45847b2b9618de900471df43d
sar06 <48696842+sar06@users.noreply.github.com>
2019-03-27 00:29:15 -0500

    Add files via upload

---
+++ ReactToDoListApp/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import "react-router";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ToDoPage from './components/ToDoPage';
+import CounterApp from './components/CounterApp';
+import "./App.css"
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+        {/*Navigation Bar*/}
+          <ul>
+            <li className="navList"><Link to="/">Home</Link></li>
+            <li className="nav"><Link to="/todopage">ToDoList</Link></li>
+            <li className="nav"><Link to="/counterapp">CounterApp</Link></li>
+          </ul>
+          <Route path="/todopage" component={ToDoPage} />
+          <Route path="/counterapp" component={CounterApp} />
+
+        </div >
+      </BrowserRouter >
+    );
+  }
+}
+
+export default App;
